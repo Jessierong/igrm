e7e333b6609dd6855536e0361dbee0dfc3df68fb
Jessie Rong <jessie.rong@ngwt.com>
2014-05-07 14:37:41 +0800

    2nd time
    remove blur... add shadow function ( click close ), fixed ie 11

--- js/script.js
+++ js/script.js
@@ -1,97 +1,109 @@
-// JavaScript Document
+ // JavaScript Document
 $(function() {
-	tabselect();
-	PageMenu(); 
-	LanguageMenu(); 
 	
-	//mobile support
-	taptouch();
-	    
-	//plugin 
-	iCheck(); 
-	$(".fancybox").fancybox();
-	$( '.datepicker' ).pickadate()
+	//initial
+    tabselect();
+    PageMenu();
+    LanguageMenu();
+	shadowClose();
+	
+    //mobile support
+    taptouch();
+
+    //plugin 
+    iCheck();
+    $(".fancybox").fancybox();
+    $('.datepicker').pickadate()
 })
 
-//win resize reload
+//window resize
 $(window).resize(function() {
-
+	toggleShadowMobile()
 });
 
-$(window).scroll(function () { 
-	//You've scrolled this much:
-	var scrollHeight = $(window).scrollTop();
-	var hearderHeight = $("header").height();
-	if ( scrollHeight > hearderHeight){
-		$("header").addClass("headfixed");
-		$("#allPart").css("padding-top",hearderHeight)
-	}
-	else{
-		$("header").removeClass("headfixed");
-		$("#allPart").css("padding-top",0)
-	}
+//header fix or not
+$(window).scroll(function() {
+    //You've scrolled this much:
+    var scrollHeight = $(window).scrollTop();
+    var hearderHeight = $("header").height();
+    if (scrollHeight > hearderHeight) {
+        $("header").addClass("headfixed");
+        $("#allPart").css("padding-top", hearderHeight)
+    } 
+    else {
+        $("header").removeClass("headfixed");
+        $("#allPart").css("padding-top", 0)
+    }
 });
-
-
-//tab select
-function tabselect(){
-	$(".tabbox h2").click(function(){
-		$(this).addClass("on").siblings().removeClass("on");		
-		var i = $(this).index();
-		$(".tabcontainer>section").hide().parent().children().eq(i).show();
-		}
-	);
-} 
  
-//slide up donw only use class name
-function slideToggle(target){ 
-	var target = "." + target;
-	$(target).slideToggle();
-	if ( $(document).width() > 480 ){	
-		if ( $(target).css("display") == "block" ){
-			$(target).css("display","");
-		}
-	}
-	shadowMobile()
+//tab select
+function tabselect() {
+    $(".tabbox h2").click(function() {
+        $(this).addClass("on").siblings().removeClass("on");
+        var i = $(this).index();
+        $(".tabcontainer>section").hide().parent().children().eq(i).show();
+    }
+    );
 }
 
-function shadowMobile(){
-	if ( $(document).width() <= 480 ){	
-		$(".wrapper-mobile").toggleClass("blur");	
-		$(".shadow-mobile").toggle();
+//slide up donw
+//menu page
+function slideTogglePage() {
+    $(".page").slideToggle();
+	if ($(document).width() <= 480) {
+		$(".shadow-mobile").fadeToggle();
 	}
+}
+
+function shadowClose(){
+	$(".shadow-mobile").click(function(){
+		$(".page").slideUp();
+		$(".shadow-mobile").fadeOut();
+	})
+}
+
+function toggleShadowMobile(){
+    if ($(document).width() <= 480 && $(".page").css("display") == "block") {
+        $(".shadow-mobile").show();
+    } else 
+    {
+        $(".shadow-mobile").hide();
+    }
+}
+//menu language
+function slideToggleLanguage() {
+    $(".language").slideToggle();
 }
 
 //menu page
-function PageMenu(){
-	$(".page li").click(function(){
-		$(".page li").removeClass("selected");
-		$(this).addClass("selected");
-	})
+function PageMenu() {
+    $(".page li").click(function() {
+        $(".page li").removeClass("selected");
+        $(this).addClass("selected");
+    })
 }
 //menu language
-function LanguageMenu(){
-	$(".language li").click(function(){
-		$(".language li").removeClass("selected").addClass("gray");
-		$(this).addClass("selected").removeClass("gray");
-	})
+function LanguageMenu() {
+    $(".language li").click(function() {
+        $(".language li").removeClass("selected").addClass("gray");
+        $(this).addClass("selected").removeClass("gray");
+    })
 }
 
 //add touch effect on element
-function taptouch(){
-	$('.form li, .button').live('touchstart', function(e){
-		$(this).addClass('active');
-	});	
-	$('.form li, .button').live('touchend', function(e){
-		$('li, .button').removeClass('active');
-	});
+function taptouch() {
+    $('.form li, .button').live('touchstart', function(e) {
+        $(this).addClass('active');
+    });
+    $('.form li, .button').live('touchend', function(e) {
+        $('li, .button').removeClass('active');
+    });
 }
 
 //icheck style initial
-function iCheck(){	
-	$('input').iCheck({
-		checkboxClass: 'icheckbox_flat-orange',
-		radioClass: 'iradio_flat-orange'
-	});      	
+function iCheck() {
+    $('input').iCheck({
+        checkboxClass: 'icheckbox_flat-orange',
+        radioClass: 'iradio_flat-orange'
+    });
 }
- 
